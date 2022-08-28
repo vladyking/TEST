8e72029eab8f157d240258df5ab3c5e0f6942c6b
vladyking <gvladykin@gmail.com>
2022-08-28 21:38:47 +0300

    First commit

---
+++ src/Test_GenerateRandomArray.java
@@ -0,0 +1,14 @@
+public class Test_GenerateRandomArray {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Test_Method.java
@@ -0,0 +1,18 @@
+public class Test_Method {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int l = 7;
+        int m = 9;
+        int c = summa(a, b);
+        int k = summa(l, m);
+        System.out.println("Сумма чисел a и b равна " + c);
+        System.out.println("Сумма чисел l и m равна " + k);
+    }
+
+    public static int summa(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+}
